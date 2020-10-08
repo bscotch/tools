58aead5f9afaa596a6569aa68962abf2253daabc
Adam Coster <adam@bscotch.net>
2020-10-08 15:22:21 -0500

    feat: Have path listing methods ignore .git and node_modules folders by default

--- src/lib/files.ts
+++ src/lib/files.ts
@@ -2,15 +2,30 @@ import fs from "fs";
 import path from "path";
 import {sortedPaths} from "./paths";
 
+export interface ListPathOptions {
+  /** By default the .git folder is skipped */
+  includeDotGit?: boolean,
+  /** By default the node_modules folder is skipped */
+  includeNodeModules?: boolean,
+}
+
 /**
  * List all files and folders in a directory. If `dir` is a
  * path, return it in an array.
  */
-export function listPathsSync(dir:string,recursive=false){
+export function listPathsSync(dir:string,recursive=false,options?:ListPathOptions){
   if(fs.statSync(dir).isFile()){
     return [dir];
   }
+  const excludedDirs:string[] = [];
+  if(!options?.includeDotGit){
+    excludedDirs.push('.git');
+  }
+  if(!options?.includeNodeModules){
+    excludedDirs.push('node_modules');
+  }
   const paths = fs.readdirSync(dir)
+    .filter(aPath=>!excludedDirs.includes(aPath))
     .map(aPath=>path.join(dir,aPath));
   if(recursive){
     const morePaths = paths
@@ -25,8 +40,8 @@ export function listPathsSync(dir:string,recursive=false){
 /**
  * List all folders in a directory.
  */
-export function listFoldersSync(dir:string,recursive=false){
-  return listPathsSync(dir,recursive)
+export function listFoldersSync(dir:string,recursive=false,options?:ListPathOptions){
+  return listPathsSync(dir,recursive,options)
     .filter(pathName=>fs.statSync(pathName).isDirectory());
 }
 
@@ -34,11 +49,11 @@ export function listFoldersSync(dir:string,recursive=false){
  * List all files in a directory or, if 'dir' is already a file,
  * just return that filename as an array.
  */
-export function listFilesSync(dir:string,recursive=false){
+export function listFilesSync(dir:string,recursive=false,options?:ListPathOptions){
   if(fs.statSync(dir).isFile()){
     return [dir];
   }
-  return listPathsSync(dir,recursive)
+  return listPathsSync(dir,recursive,options)
     .filter(filePath=>fs.statSync(filePath).isFile());
 }
 
@@ -46,9 +61,9 @@ export function listFilesSync(dir:string,recursive=false){
  * List all files in a directory or, if 'dir' is already a file,
  * just return that filename as an array.
  */
-export function listFilesByExtensionSync(dir:string,extension:string|string[],recursive=false){
+export function listFilesByExtensionSync(dir:string,extension:string|string[],recursive=false,options?:ListPathOptions){
   const extensions = Array.isArray(extension) ? extension : [extension];
-  return listFilesSync(dir,recursive)
+  return listFilesSync(dir,recursive,options)
     .filter(fileName=>{
       const ext = path.parse(fileName).ext.slice(1);
       return extensions.includes(ext);

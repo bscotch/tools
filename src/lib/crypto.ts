e93a91a684747ce7de67601c869ff6536b1da2bd
Adam Coster <adam@bscotch.net>
2020-12-16 15:39:32 -0600

    fix: Types for the crypto node module have a breaking change.

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -1,21 +1,22 @@
-import crypto, { BinaryLike, HexBase64Latin1Encoding } from "crypto";
+import crypto, { BinaryLike, Encoding } from "crypto";
 
-export function createHash (algorithm:string,source:BinaryLike,encoding:HexBase64Latin1Encoding="hex"){
+export function createHash (algorithm:string,source:BinaryLike,encoding:Encoding="hex"){
   return crypto
     .createHash(algorithm)
     .update(source)
+    // @ts-ignore
     .digest(encoding);
 }
 
-export function sha1 (source:BinaryLike,encoding:HexBase64Latin1Encoding="hex"){
+export function sha1 (source:BinaryLike,encoding:Encoding="hex"){
   return createHash("sha1",source,encoding);
 }
 
-export function sha256 (source:BinaryLike,encoding:HexBase64Latin1Encoding="hex"){
+export function sha256 (source:BinaryLike,encoding:Encoding="hex"){
   return createHash("sha256",source,encoding);
 }
 
 
-export function md5 (source:BinaryLike,encoding:HexBase64Latin1Encoding="hex"){
+export function md5 (source:BinaryLike,encoding:Encoding="hex"){
   return createHash("md5",source,encoding);
 }
